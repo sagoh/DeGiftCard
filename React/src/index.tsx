11e6049c4cb38e57d051c9de8a464dffa5fade6b
sagoh <sampsongoh1@gmail.com>
2021-11-29 13:04:36 +0800

    Update index.tsx

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -7,7 +7,7 @@ import { ChainId, Config, DAppProvider } from '@usedapp/core';
 const client = new QueryClient();
 
 const alchemyKey =
-	process.env.REACT_APP_ALCHEMY_KEY || `-hGaD3Kdebo8_Q5fYjPEUkn6qTjcKKoR`;
+	process.env.REACT_APP_ALCHEMY_KEY ;
 
 const config = {
 	readOnlyChainId: ChainId.Rinkeby,
